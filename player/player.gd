b3a147b4d26725f8dce4372d0d066a3ccebd0966
Logan Croley <logan.croley.18@gmail.com>
2024-09-16 19:15:05 -0400

    moving the player

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
+@export var speed = 600
+
 func _ready() -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	pass
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	move_and_slide()
