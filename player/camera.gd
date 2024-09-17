a0e215a8e5bddbe49f746d6174259e79bc063239
Logan Croley <logan.croley.18@gmail.com>
2024-09-16 22:18:12 -0400

    fix: removed the jitter that was on the character

--- player/camera.gd
+++ player/camera.gd
@@ -10,20 +10,20 @@ var last_physics_pos
 func lean_camera_towards_mouse_(delta:float) -> void:
 	var mouse_position := get_global_mouse_position()
 	
-	var direction_to_mouse := (mouse_position - position).normalized()
-	var distance_to_mouse :=  mouse_position.distance_to(position)
-	var lean := (mouse_position - position) * scale_lean
+	var direction_to_mouse := (mouse_position - global_position).normalized()
+	var distance_to_mouse :=  mouse_position.distance_to(global_position)
+	var lean := (mouse_position - global_position) * scale_lean
 	offset = lerp(offset, lean, delta * smooth_lean)
 
 func match_player_position_() -> void:
-	position = player.position
+	global_position = player.global_position
 	
 func _process(delta) -> void:
 	if Engine.get_frames_per_second() > 60: # This is whatever your physics fps is
 		var lerp_weight = Engine.get_physics_interpolation_fraction()
-		position = last_physics_pos.lerp(player.position, lerp_weight)
+		global_position = last_physics_pos.lerp(player.global_position, lerp_weight)
 
 func _physics_process(delta) -> void:
 	lean_camera_towards_mouse_(delta)
-	last_physics_pos = position
+	last_physics_pos = global_position
 	match_player_position_()
