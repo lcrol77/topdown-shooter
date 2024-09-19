268525aee8b32e3a57781a4db19e993aaaf5a36c
Logan Croley <logan.croley.18@gmail.com>
2024-09-18 23:05:37 -0400

    added simple shooting

--- guns/test/gun.gd
+++ guns/test/gun.gd
@@ -3,10 +3,16 @@ class_name Gun
 
 @onready var bullet_spawn: Node2D = $BulletSpawn
 @export var is_active: bool = false
+const bullet = preload("res://guns/test/bullet.tscn")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("fire"):
-		print("pow")
+		if is_active:
+			var instance = bullet.instantiate()
+			instance.global_position = bullet_spawn.global_position
+			instance.rotation = rotation
+			instance.target = get_global_mouse_position()
+			get_parent().add_child(instance)
 		
 	if event.is_action_released("fire"):
 		print("released")
